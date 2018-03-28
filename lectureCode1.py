8c6017cb74e08898aa623d87cbfe8d4e0b2b6d14
LeoGillet <meowington.lg@gmail.com>
2018-03-28 11:57:29 +0200

    28/03

--- lectureCode1.py
+++ lectureCode1.py
@@ -8,20 +8,18 @@ tailleLigne = 12
 file ='base_donnees.txt'
 
 def choixCode():
-
-    t1 = time()
-
-    fichier = open(file,'r')#on ouvre le fichier en mode lecture
-    nombreLigne = (os.path.getsize(file)+2)/12#on détermine le nombre de lignes du fichier
-    random = randint(0,nombreLigne-1)#on génère aléatoirement un numéro de ligne
-    fichier.seek(tailleLigne*random)#on change la position du curseur
-    maLigne = fichier.readline().strip()#on enlève les caractères spéciaux en fin de ligne
+    #t1 = time()
+    fichier = open(file,'r')                #on ouvre le fichier en mode lecture
+    nombreLigne = (os.path.getsize(file)+2)/tailleLigne  #on détermine le nombre de lignes du fichier
+    random = randint(0,nombreLigne-1)       #on génère aléatoirement un numéro de ligne
+    fichier.seek(tailleLigne*random)        #on change la position du curseur
+    maLigne = fichier.readline().strip()    #on enlève les caractères spéciaux en fin de ligne
     fichier.close
 
-    print('temps =',time()-t1)#on affiche le temps d'exécution
+    #print('temps =',time()-t1)              #on affiche le temps d'exécution
     #print(random)
     #print(nombreLigne)
-    return maLigne#on renvoie une ligne aléatoire du fichier .txt
+    return maLigne                          #on renvoie une ligne aléatoire du fichier .txt
 
 
 ##choixCode()
--- script_validite.py
+++ script_validite.py
@@ -1,31 +1,22 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Jan 24 10:31:52 2018
-
-@author: leo.gillet
-"""
-
 import sqlite3
+from lectureCode1 import choixCode
 
 conn = sqlite3.connect('Datab.db')
 c = conn.cursor()
 
-#Exemple d'identifiant : 1003920006
-identifiant = input('Entrez l\'identifiant : ')
+def rechercheIdentifiant(idCarte):
+    c.execute("SELECT identifiant, nom, prenom FROM ids")
+    liste_ids = c.fetchall()
+    try:
+        validite = True
+        i = 0
+        while liste_ids[i][0] != idCarte:
+            i += 1
+        sortie = 'IDENTIFIANT : '+str(idCarte)+' EST TROUVE A LA LIGNE : '+str(i+1)+' ASSOCIE A : '+str(liste_ids[i][1])+' '+str(liste_ids[i][2])
+    except IndexError:
+        validite = False
+        sortie = """!! L'identifiant n'est pas présent dans la base de données !!"""
+    print(sortie)
+    return sortie
 
-c.execute("SELECT identifiant, nom, prenom FROM ids")
-liste_ids = c.fetchall()
-print('La base de données comporte',len(liste_ids),'lignes.')
-
-try:
-    validite = True
-    i = 0
-    while liste_ids[i][0] != identifiant:
-        i+=1
-except IndexError:
-    print("""!! L'identifiant n'est pas présent dans la base de données !!""")
-    validite = False
-
-print('')
-print('Identifiant :',identifiant)
-print('Trouvé à la ligne',i+1,':',liste_ids[i][1],liste_ids[i][2])
+rechercheIdentifiant(choixCode())
